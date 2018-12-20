122ec72626d37014f99afe98cbb0c68f39645565
Pouya <pouyaheydary@gmail.com>
2018-12-20 16:37:30 +0330

    added json Object

--- app/src/main/java/ir/mftvanak/mftfridays/TestWebActivity.java
+++ app/src/main/java/ir/mftvanak/mftfridays/TestWebActivity.java
@@ -16,13 +16,13 @@ public class TestWebActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_web);
 
-
+final String city = "tehran";
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
 
-                    URL obj = new URL("https://google.com");
+                    URL obj = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city+"%2C%20ir%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
 
                     HttpURLConnection con = (HttpURLConnection) obj.openConnection();
                     con.setRequestMethod("GET");
@@ -40,7 +40,6 @@ public class TestWebActivity extends AppCompatActivity {
                         while ((inputLine = in.readLine()) != null) {
                             response.append(inputLine);
                         }
-
 
                         System.out.println(response.toString());
                     }
